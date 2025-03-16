430733a61d5b0d987b584abd0178d1dfcaccb0de
marwanmohamedd7 <maro.jk2345@gmail.com>
2025-03-16 05:28:44 +0200

    build: website pages

---
+++ app/account/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Guest Area",
+};
+
+function Page() {
+  return (
+    <h2 className="font-semibold text-2xl capitalize text-accent-400 mb-7">
+      welcome, marwan
+    </h2>
+  );
+}
+
+export default Page;
