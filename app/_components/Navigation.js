848eb2f93f14b5081d94b6e3ab761607f5d89000
marwanmohamedd7 <maro.jk2345@gmail.com>
2025-06-13 00:40:38 +0300

    fix reservation calendar bug && optimize images

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -4,6 +4,7 @@ import { auth } from "../_lib/auth";
 
 export default async function Navigation() {
   const session = await auth();
+  // const session = "";
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
